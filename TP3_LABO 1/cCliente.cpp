4b686d7329a5ee92eda99b0535b279ae10a856d4
Juan Estrella <manuestrella2002@gmail.com>
2021-05-19 18:37:13 -0300

    Inicialización de clases(cAlquiler, cCliente, cEmpresa, cTraffic)

--- TP3_LABO 1/cCliente.cpp
+++ TP3_LABO 1/cCliente.cpp
@@ -1,1 +1,18 @@
 #include "cCliente.h"
+
+cCliente::cCliente(string Nombre_, string Apellido_, string DNI)
+{
+}
+
+cCliente::~cCliente()
+{
+}
+
+string cCliente::To_String()
+{
+	return string();
+}
+
+void cCliente::Imprimir()
+{
+}
